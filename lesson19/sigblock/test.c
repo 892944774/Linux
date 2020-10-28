2da892b9b6c33cc92c16c83a2bf271dd0936ba51
zs <zs@localhost.localdomain>
2020-10-28 16:23:58 +0800

    信号+进程1

---
+++ lesson19/sigblock/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+
+void sigcb(int signo)
+{
+    printf("signo is %d\n", signo);
+}
+
+int main()
+{
+    signal(2, sigcb);
+    signal(40, sigcb);
+
+    sigset_t newset,oldset;
+    sigemptyset(&oldset);
+
+    sigfillset(&newset);
+    sigprocmask(SIG_SETMASK, &newset, &oldset);
+    getchar();
+
+    //解除阻塞
+    sigprocmask(SIG_SETMASK, &oldset, NULL);
+    //观察2号信号调用了几次sigcb
+    //40号信号调用了几次sigcb
+    while(1)
+    {
+        sleep(1);
+    }
+    return 0;
+}
+
